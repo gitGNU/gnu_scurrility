05e27ab8e5246af09c98e27c9277ed9669d9ff20
Thomas Cort <tcort@tomcort.com>
2009-04-11 20:22:42 -0400

    Add example client in perl

---
+++ clients/perl/client.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl -w
+#
+# Copyright (C) 2009 Thomas Cort <tcort@tomcort.com>
+#
+# This file is part of scurrility.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+use SOAP::Lite;
+
+sub filter {
+	my($msg) = @_;
+
+	return SOAP::Lite
+		-> uri('http://www.scurrility.ws/scurrility')
+		-> proxy('http://www.scurrility.ws/scurrility/scurrility.php')
+		-> Scurrility($msg)
+		-> result . "\n";
+}
+
+print &filter('go to hell');
